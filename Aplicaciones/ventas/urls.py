3874febeb333104a879b2d5afd504ebcffeeb071
acostamarianela <acostagm6@gmail.com>
2024-10-17 19:30:23 -0300

    para facturear

--- Aplicaciones/ventas/urls.py
+++ Aplicaciones/ventas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'servicios'
+app_name = 'ventas'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('factura-prueba/', views.enviar_factura_prueba, name='factura-prueba')
 ]
