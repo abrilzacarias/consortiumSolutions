c080fc2a35482ecea8f8bded34a32486ec479538
acostamarianela <acostagm6@gmail.com>
2024-10-19 23:42:09 -0300

    genera links mp

--- Aplicaciones/facturas/views.py
+++ Aplicaciones/facturas/views.py
@@ -1,21 +1,57 @@
 from django.shortcuts import render
 from .models import Facturas
-# Create your views here.
+import mercadopago
 
 def listarFacturas(request):
     resultados = Facturas.listarFacturas()
     
     facturas = {
-    str(entry[0]): {  # Usar el numero_comprobante como clave
-        'numero_comprobante': entry[1],
-        'fecha_emision': entry[2],
-        'nombre_cliente': entry[3],
-        'apellido_cliente': entry[4],
-        'nombre_edificio': entry[5],
-        'descarga_ticket': entry[6]
+        str(entry[0]): {  # Usar el numero_comprobante como clave
+            'numero_comprobante': entry[1],
+            'fecha_emision': entry[2],
+            'nombre_cliente': entry[3],
+            'apellido_cliente': entry[4],
+            'nombre_edificio': entry[5],
+            'descarga_ticket': entry[6]
+        }
+        for entry in resultados
     }
-    for entry in resultados
-    }
-    print(facturas)
 
-    return render(request, 'vistaFacturas.html', {'facturas': facturas}) 
+    # Llama a la función para generar el link de pago
+    payment_link = generar_link_pago()
+
+    return render(request, 'vistaFacturas.html', {'facturas': facturas, 'payment_link': payment_link}) 
+
+def generar_link_pago():
+    sdk = mercadopago.SDK("TEST-1083456035276298-101922-810a2e0aeb770bbf8ed3191b3cd59702-566328219")
+    
+    # Crea un objeto de preferencia de pago
+    preference_data = {
+        "items": [
+            {
+                "title": "Producto de ejemplo",
+                "quantity": 1,
+                "unit_price": 100,
+                "currency_id": "ARS"
+            }
+        ],
+        "payer": {
+            "email": "test_user_123456@testuser.com"
+        }
+    }
+
+    # Realiza la solicitud para crear la preferencia de pago
+    preference = sdk.preference().create(preference_data)
+
+    # Imprime la respuesta completa para depuración
+    print("Respuesta de Mercado Pago:", preference)
+
+    # Obtén el link de pago si existe
+    payment_link = preference["response"].get("init_point")
+
+    if payment_link:
+        print("Link de Pago:", payment_link)
+    else:
+        print("No se pudo obtener el link de pago.")
+    
+    return payment_link
