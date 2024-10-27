3b0ae474c77f6daf1fce006b3392d1e0a6cfb7c3
hibren <canobren080603@gmail.com>
2024-10-27 19:23:56 -0300

    anda filtrosd de factura

--- Aplicaciones/facturas/static/filtrarFacturas.js
+++ Aplicaciones/facturas/static/filtrarFacturas.js
@@ -1,57 +1,47 @@
-// Asegurarse de que el DOM esté completamente cargado antes de ejecutar el código
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtener los botones y los elementos de cliente
+    // Obtener los botones y los elementos de factura
     const btnTodos = document.getElementById('btn-todos');
     const btnPendientes = document.getElementById('btn-pendientes');
     const btnProceso = document.getElementById('btn-proceso');
     const btnPagados = document.getElementById('btn-pagados');
-    const facturas = document.querySelectorAll('.border-b-factura'); // Seleccionamos todos los clientes
+    const facturas = document.querySelectorAll('.border-b-factura');
 
-    // Función para mostrar todos los clientes
+    // Función para mostrar todos las facturas
     btnTodos.addEventListener('click', () => {
-        facturas.forEach(cliente => {
-            factura.style.display = 'table-row'; // Mostrar todos
+        facturas.forEach(factura => {
+            factura.style.display = 'table-row';
         });
     });
 
-    // Función para mostrar solo prospectos
+    // Función para mostrar solo pendientes
     btnPendientes.addEventListener('click', () => {
         facturas.forEach(factura => {
             if (factura.getAttribute('data-tipo') === '1') {
-                factura.style.display = 'table-row'; // Mostrar prospectos
+                factura.style.display = 'table-row';
             } else {
-                factura.style.display = 'none'; // Ocultar otros
-            }
-        });
-    });
-    // Función para mostrar solo prospectos
-    btnPagados.addEventListener('click', () => {
-        facturas.forEach(factura => {
-            if (factura.getAttribute('data-tipo') === '2') {
-                factura.style.display = 'table-row'; // Mostrar prospectos
-            } else {
-                factura.style.display = 'none'; // Ocultar otros
+                factura.style.display = 'none';
             }
         });
     });
 
-    // Función para mostrar solo prospectos
-    btnProceso.addEventListener('click', () => {
+    // Función para mostrar solo pagados
+    btnPagados.addEventListener('click', () => {
         facturas.forEach(factura => {
-            if (factura.getAttribute('data-tipo') === '3') {
-                factura.style.display = 'table-row'; // Mostrar prospectos
+            if (factura.getAttribute('data-tipo') === '2') {
+                factura.style.display = 'table-row';
             } else {
-                factura.style.display = 'none'; // Ocultar otros
+                factura.style.display = 'none';
             }
         });
     });
-    // Función para mostrar solo clientes
-    btnClientes.addEventListener('click', () => {
-        clientes.forEach(cliente => {
-            if (cliente.getAttribute('data-tipo') === '1') {
-                cliente.style.display = 'table-row'; // Mostrar clientes
+
+    // Función para mostrar solo en proceso
+    btnProceso.addEventListener('click', () => {
+        facturas.forEach(factura => {
+            if (factura.getAttribute('data-tipo') === '3') {
+                factura.style.display = 'table-row';
             } else {
-                cliente.style.display = 'none'; // Ocultar otros
+                factura.style.display = 'none';
             }
         });
     });
