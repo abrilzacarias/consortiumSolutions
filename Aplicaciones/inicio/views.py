c91959b751050c0c31d315a8b86a8f158a66ecc3
acostamarianela <acostagm6@gmail.com>
2024-11-14 23:42:14 -0300

    cliente que mas compra

--- Aplicaciones/inicio/views.py
+++ Aplicaciones/inicio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Actividades
+from .models import Actividades, Graficos
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
