2ba78b1a77bc83fc56c6b5380cab46d1cb682c0a
acostamarianela <acostagm6@gmail.com>
2024-12-10 18:17:12 -0300

    no genera mas link de pago

--- Aplicaciones/facturas/urls.py
+++ Aplicaciones/facturas/urls.py
@@ -5,6 +5,5 @@ app_name =  'facturas'
 urlpatterns = [
     path('', views.listarFacturas, name='listarFacturas'),
     path('actualizar_estado_factura/<int:id_factura>/', views.actualizar_estado_factura, name='actualizar_estado_factura'),
-    path('generar_link_pago/<int:id_factura>/', views.generar_link_pago, name='generar_link_pago'),
    
 ]
--- Aplicaciones/facturas/views.py
+++ Aplicaciones/facturas/views.py
@@ -30,71 +30,7 @@ def listarFacturas(request):
     
     return render(request, 'vistaFacturas.html', context)
 
-@login_required
-def generar_link_pago(request, id_factura):
-    try:
-        facturas = Factura.listarFacturas()
-        vista_factura = None
-        for factura in facturas:
-            if factura['id_factura'] == id_factura:  
-                vista_factura = factura
-                break
-        
-        if not vista_factura:
-            raise Http404("Factura no encontrada")
-        
-        nombre_cliente = vista_factura['nombre_cliente']
-        apellido_cliente = vista_factura['apellido_cliente']
-        descarga_ticket = vista_factura['descarga_ticket']
-        
-        # Obtén los detalles de los servicios y divide en listas
-        detalles_servicios = vista_factura['detalles']
-        
-        items = []
-        for detalle in detalles_servicios:
-            nombres_servicios = detalle['nombre_servicio'].split(", ")
-            cantidades_servicios = detalle['cantidad_servicio'].split(", ")
-            precios_totales_servicios = detalle['precio_total_servicio'].split(", ")
-            
-            for nombre, cantidad, precio in zip(nombres_servicios, cantidades_servicios, precios_totales_servicios):
-                items.append({
-                    "title": nombre,
-                    "quantity": 1,
-                    "unit_price": float(precio),
-                    "currency_id": "ARS"
-                })
 
-        # Configuración de Mercado Pago
-        sdk = mercadopago.SDK("TEST-1083456035276298-101922-810a2e0aeb770bbf8ed3191b3cd59702-566328219")
-
-        # Datos de la preferencia
-        preference_data = {
-            "items": items,
-            "payer": {
-                "email": vista_factura['detalles'][0]['correo_cliente']
-            }
-        }
-
-        # Crear preferencia
-        preference = sdk.preference().create(preference_data)
-        payment_link = preference["response"].get("init_point")
-
-        # Enviar correo con el link de pago
-        Factura.enviar_correo_link_pago(
-            vista_factura['detalles'][0]['correo_cliente'],
-            payment_link,
-            nombre_cliente,
-            apellido_cliente,
-            descarga_ticket
-        )
-        
-        messages.success(request, f"El link de pago se envió correctamente. Link: {payment_link}")
-        return redirect(reverse('facturas:listarFacturas'))
-        
-    except Exception as e:
-        print(f"Error detallado: {str(e)}")
-        messages.error(request, f"Ocurrió un error: {str(e)}")
-        return redirect(reverse('facturas:listarFacturas'))
 
 @login_required
 def actualizar_estado_factura(request, id_factura):
